7f6994736621e3f1c1684a672c8d781de3d31bcf
youth <youthhou@163.com>
2018-02-04 21:40:08 +0800

    m

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-import numpy
-from Cython.Distutils import build_ext
-
-setup(
-    cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("DCD", sources=["_DCD.pyx", "DCD.c"], include_dirs=[numpy.get_include()])],)
